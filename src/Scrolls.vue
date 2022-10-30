79818709d9629c92fd6bd445adebc25184d5f74b
MichaeLee1 <cspkno005@gmail.com>
2022-10-30 12:16:52 +0800

    doing keyboard scrolls

--- src/Scrolls.vue
+++ src/Scrolls.vue
@@ -1,55 +1,105 @@
 <script setup>
 import { onMounted, ref } from "vue";
+const dom = ref();
 
-// 鍵盤觸發event事件，window為全畫面媒介，切換1 ~ 8。
+//圖檔位置，對應數字 1 ~ 8
+const slots = ref([
+  {
+    id: 0,
+    title: "對武器施法的卷軸",
+    url: "/src/assets/scroll_white-weapon.png",
+  },
+  {
+    id: 1,
+    title: "祝福的對武器施法的卷軸",
+    url: "/src/assets/scroll_blessed-weapon.png",
+  },
+  {
+    id: 2,
+    title: "受詛咒的對武器施法的卷軸",
+    url: "/src/assets/scroll_cursed-weapon.png",
+  },
+  {
+    id: 3,
+    title: "空格",
+    url: "/src/assets/scroll_empty-slot.jpg",
+  },
+  {
+    id: 4,
+    title: "對盔甲施法的卷軸",
+    url: "/src/assets/scroll_white-armor.png",
+  },
+  {
+    id: 5,
+    title: "祝福的對盔甲施法的卷軸",
+    url: "/src/assets/scroll_blessed-armor.png",
+  },
+  {
+    id: 6,
+    title: "受詛咒的對盔甲施法的卷軸",
+    url: "/src/assets/scroll_cursed-armor.png",
+  },
+  {
+    id: 7,
+    title: "空格",
+    url: "/src/assets/scroll_empty-slot.jpg",
+  },
+]);
+
 onMounted(() => {
+  // 為了一開始就跑空白預設樣式，所以在mounted時，新增class
+  dom.value.children[3].classList.add("empty-default");
+  dom.value.children[7].classList.add("empty-default");
+
+  // 鍵盤觸發event事件，window為全畫面媒介，切換1 ~ 8。
   window.addEventListener("keypress", (e) => {
     // 把dom節點轉成Array好處理個別資料
-    const nodes = Array.from(e.target.children[0].children[0].children[2].children);
-    console.log(nodes);
-    nodes.forEach((node) => {
-      node.classList.remove("active");
+    const slotListKeyboard = Array.from(
+      e.target.children[0].children[0].children[2].children
+    );
+    slotListKeyboard.forEach((slotKeyboard) => {
+      slotKeyboard.classList.remove("active");
     });
     switch (e.key) {
       // 按鍵1，白武 +1
       case "1":
         console.log("白武，參數+1");
-        nodes[0].classList.add("active");
+        slotListKeyboard[0].classList.add("active");
         break;
       // 按鍵2，祝武 +1 ~ +3
       case "2":
         console.log("祝武，參數+1 - 3");
-        nodes[1].classList.add("active");
+        slotListKeyboard[1].classList.add("active");
         break;
       // 按鍵3，紅武 -1
       case "3":
         console.log("紅武，參數-1");
-        nodes[2].classList.add("active");
+        slotListKeyboard[2].classList.add("active");
         break;
       // 按鍵4，空格
       case "4":
         console.log("空格");
-        nodes[3].classList.add("active");
+        slotListKeyboard[3].classList.add("active");
         break;
       // 按鍵5，白防 ＋1
       case "5":
         console.log("白防，參數+1");
-        nodes[4].classList.add("active");
+        slotListKeyboard[4].classList.add("active");
         break;
       // 按鍵6，祝防 +1 ~ +3
       case "6":
         console.log("祝防，參數+1 - 3");
-        nodes[5].classList.add("active");
+        slotListKeyboard[5].classList.add("active");
         break;
       // 按鍵7，紅防 -1
       case "7":
         console.log("紅防，參數-1");
-        nodes[6].classList.add("active");
+        slotListKeyboard[6].classList.add("active");
         break;
       // 按鍵8，空格
       case "8":
         console.log("空格");
-        nodes[7].classList.add("active");
+        slotListKeyboard[7].classList.add("active");
         break;
       // 其他，不回應
       default:
@@ -58,15 +108,16 @@ onMounted(() => {
     }
   });
 });
+
 // 滑鼠觸發event事件，img tag為媒介，切換1 ~ 8。
 function mouseToggleActive(event) {
   const target = event.target;
-  const slots = target.parentElement.querySelectorAll("img");
+  const slotListMouse = target.parentElement.querySelectorAll("img");
 
-  slots.forEach((slot) => {
-    // 為了切換樣式功能，先把全部node的active移除
-    slot.classList.remove("active");
-    if (slot.classList === target.classList) {
+  slotListMouse.forEach((slotMouse) => {
+    // 為了切換樣式功能，把全部的active移除
+    slotMouse.classList.remove("active");
+    if (slotMouse.classList === target.classList) {
       target.classList.add("active");
 
       // 透過slice切割網址，針對不同情況給不同係數
@@ -95,59 +146,24 @@ function mouseToggleActive(event) {
         case "cursed-armor.png":
           console.log("紅防，參數-1");
           break;
-        // 空格 沒有任何事
+        // 空格
         default:
-          console.log("空格，沒有事情");
+          console.log("空格");
           break;
       }
     }
   });
 }
-
-const scrolls = ref([
-  {
-    id: 0,
-    title: "對武器施法的卷軸",
-    url: "/src/assets/scroll_white-weapon.png",
-  },
-  {
-    id: 1,
-    title: "祝福的對武器施法的卷軸",
-    url: "/src/assets/scroll_blessed-weapon.png",
-  },
-  {
-    id: 2,
-    title: "受詛咒的對武器施法的卷軸",
-    url: "/src/assets/scroll_cursed-weapon.png",
-  },
-  {
-    id: 3,
-    title: "對盔甲施法的卷軸",
-    url: "/src/assets/scroll_white-armor.png",
-  },
-  {
-    id: 4,
-    title: "祝福的對盔甲施法的卷軸",
-    url: "/src/assets/scroll_blessed-armor.png",
-  },
-  {
-    id: 5,
-    title: "受詛咒的對盔甲施法的卷軸",
-    url: "/src/assets/scroll_cursed-armor.png",
-  },
-]);
 </script>
 
 <template>
-  <div class="container" @click="defaultActive">
+  <div ref="dom" class="container" @click="defaultActive">
     <img
-      v-for="scroll in scrolls"
-      :key="scroll.id"
-      :src="scroll.url"
+      v-for="slot in slots"
+      :key="slot.id"
+      :src="slot.url"
       @click="mouseToggleActive"
     />
-    <img src="./assets/scroll_empty-slot.jpg" @click="mouseToggleActive" class="f8" />
-    <img src="./assets/scroll_empty-slot.jpg" @click="mouseToggleActive" class="f12" />
   </div>
 </template>
 
@@ -166,14 +182,8 @@ img {
   width: 34px;
   height: 34px;
 }
-.f8 {
-  grid-column: 4/5;
-  grid-row: 1/2;
-  opacity: 0;
-}
-.f12 {
-  grid-column: 4/5;
-  grid-row: 2/3;
+
+.empty-default {
   opacity: 0;
 }
 .active[data-v-613df293] {
