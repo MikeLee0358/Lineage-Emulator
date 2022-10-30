6121d1f9398852fb5de5b7597995cfb692735dfb
MichaeLee1 <cspkno005@gmail.com>
2022-10-30 17:22:00 +0800

    done refactor code

--- src/Scrolls.vue
+++ src/Scrolls.vue
@@ -1,14 +1,109 @@
 <script setup>
 import { onMounted, ref } from "vue";
-// 抓取div的節點，主要是empty-slot加上樣式。
+// 抓取<div class="container">節點，加工成nodeList，就會涵蓋slot 1 ~ 8的節點陣列，之後資料都是用這個來操作
 const dom = ref();
+
 onMounted(() => {
-  // 為了一開始就跑空白預設樣式，所以在mounted時，新增class
-  dom.value.children[0].classList.add("empty-default");
-  dom.value.children[4].classList.add("empty-default");
+  const nodeList = Array.from(dom.value.children);
+  nodeList[0].classList.add("empty-default");
+  nodeList[4].classList.add("empty-default");
   window.addEventListener("keypress", keyboardToggleActive);
 });
+function keyboardToggleActive(e) {
+  const nodeList = Array.from(dom.value.children);
 
+  nodeList.forEach((node) => {
+    node.classList.remove("active");
+  });
+  switch (e.key) {
+    // 按鍵1，白武 +1
+    case "1":
+      console.log("空格");
+      nodeList[0].classList.add("active");
+      break;
+    // 按鍵2，祝武 +1 ~ +3
+    case "2":
+      console.log("白武，參數+1");
+      nodeList[1].classList.add("active");
+      break;
+    // 按鍵3，紅武 -1
+    case "3":
+      console.log("祝武，參數+1 - 3");
+      nodeList[2].classList.add("active");
+      break;
+    // 按鍵4，空格
+    case "4":
+      console.log("紅武，參數-1");
+
+      nodeList[3].classList.add("active");
+      break;
+    // 按鍵5，白防 ＋1
+    case "5":
+      console.log("空格");
+      nodeList[4].classList.add("active");
+      break;
+    // 按鍵6，祝防 +1 ~ +3
+    case "6":
+      console.log("白防，參數+1");
+      nodeList[5].classList.add("active");
+      break;
+    // 按鍵7，紅防 -1
+    case "7":
+      console.log("祝防，參數+1 - 3");
+      nodeList[6].classList.add("active");
+      break;
+    // 按鍵8，空格
+    case "8":
+      console.log("紅防，參數-1");
+      nodeList[7].classList.add("active");
+      break;
+    // 其他，不回應
+    default:
+      console.log("其他按鍵");
+      break;
+  }
+}
+function mouseToggleActive(e) {
+  const nodeList = Array.from(dom.value.children);
+  const target = e.target;
+
+  nodeList.forEach((node) => {
+    node.classList.remove("active");
+    if (node.classList === target.classList) {
+      target.classList.add("active");
+      switch (target.src.slice(40)) {
+        // 白武 +1
+        case "white-weapon.png":
+          console.log("白武，參數+1");
+          break;
+        // 祝武 +1 ~ +3
+        case "blessed-weapon.png":
+          console.log("祝武，參數+1 - 3");
+          break;
+        // 紅武 -1
+        case "cursed-weapon.png":
+          console.log("紅武，參數-1");
+          break;
+        // 白防 ＋1
+        case "white-armor.png":
+          console.log("白防，參數+1");
+          break;
+        // 祝防 +1 ~ +3
+        case "blessed-armor.png":
+          console.log("祝防，參數+1 - 3");
+          break;
+        // 紅防 -1
+        case "cursed-armor.png":
+          console.log("紅防，參數-1");
+          break;
+        // 空格
+        default:
+          console.log("空格");
+          break;
+      }
+    }
+  });
+}
 //圖檔位置，對應數字 1 ~ 8
 const slotList = ref([
   {
@@ -52,109 +147,6 @@ const slotList = ref([
     url: "/src/assets/scroll_cursed-armor.png",
   },
 ]);
-// 鍵盤觸發event事件，window為全畫面媒介，切換1 ~ 8。
-function keyboardToggleActive(e) {
-  // 把dom節點轉成Array好處理個別資料
-  const slotListKeyboard = Array.from(
-    e.target.children[0].children[0].children[2].children
-  );
-  console.log("keyboard", slotListKeyboard);
-  slotListKeyboard.forEach((slotKeyboard) => {
-    slotKeyboard.classList.remove("active");
-  });
-  switch (e.key) {
-    // 按鍵1，白武 +1
-    case "1":
-      console.log("空格");
-      slotListKeyboard[0].classList.add("active");
-      break;
-    // 按鍵2，祝武 +1 ~ +3
-    case "2":
-      console.log("白武，參數+1");
-      slotListKeyboard[1].classList.add("active");
-      break;
-    // 按鍵3，紅武 -1
-    case "3":
-      console.log("祝武，參數+1 - 3");
-      slotListKeyboard[2].classList.add("active");
-      break;
-    // 按鍵4，空格
-    case "4":
-      console.log("紅武，參數-1");
-
-      slotListKeyboard[3].classList.add("active");
-      break;
-    // 按鍵5，白防 ＋1
-    case "5":
-      console.log("空格");
-      slotListKeyboard[4].classList.add("active");
-      break;
-    // 按鍵6，祝防 +1 ~ +3
-    case "6":
-      console.log("白防，參數+1");
-      slotListKeyboard[5].classList.add("active");
-      break;
-    // 按鍵7，紅防 -1
-    case "7":
-      console.log("祝防，參數+1 - 3");
-      slotListKeyboard[6].classList.add("active");
-      break;
-    // 按鍵8，空格
-    case "8":
-      console.log("紅防，參數-1");
-      slotListKeyboard[7].classList.add("active");
-      break;
-    // 其他，不回應
-    default:
-      console.log("其他按鍵");
-      break;
-  }
-}
-// 滑鼠觸發event事件，img tag為媒介，切換1 ~ 8。
-function mouseToggleActive(e) {
-  const target = e.target;
-  const slotListMouse = target.parentElement.querySelectorAll("img");
-  console.log("mouse", slotListMouse);
-  slotListMouse.forEach((slotMouse) => {
-    // 為了切換樣式功能，把全部的active移除
-    slotMouse.classList.remove("active");
-    if (slotMouse.classList === target.classList) {
-      target.classList.add("active");
-
-      // 透過slice切割網址，針對不同情況給不同係數
-      switch (target.src.slice(40)) {
-        // 白武 +1
-        case "white-weapon.png":
-          console.log("白武，參數+1");
-          break;
-        // 祝武 +1 ~ +3
-        case "blessed-weapon.png":
-          console.log("祝武，參數+1 - 3");
-          break;
-        // 紅武 -1
-        case "cursed-weapon.png":
-          console.log("紅武，參數-1");
-          break;
-        // 白防 ＋1
-        case "white-armor.png":
-          console.log("白防，參數+1");
-          break;
-        // 祝防 +1 ~ +3
-        case "blessed-armor.png":
-          console.log("祝防，參數+1 - 3");
-          break;
-        // 紅防 -1
-        case "cursed-armor.png":
-          console.log("紅防，參數-1");
-          break;
-        // 空格
-        default:
-          console.log("空格");
-          break;
-      }
-    }
-  });
-}
 </script>
 
 <template>
