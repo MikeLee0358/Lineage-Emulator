16c8478785b6aab288aa0a8950c219392b0bc2d7
MichaeLee1 <cspkno005@gmail.com>
2022-11-05 02:37:09 +0800

    done hover with click & remad css constructor

--- src/components/SlotList.vue
+++ src/components/SlotList.vue
@@ -1,22 +1,20 @@
 <template>
-  <ul ref="iNeedThisNode" class="container">
+  <ul ref="nodeForArray">
     <li
       v-for="slot in slotList"
       :key="slot.id"
       :class="slot.name"
-      class="infoClass"
-      :data-showInfo="getInfo(slot.name, slot.toDisplay)"
+      class="infoTemplate"
+      :data-showInfo="getInfoData(slot.name, slot.toDisplay)"
     >
       <img :src="checkSrc(slot.src)" @click="clickToActive" />
     </li>
   </ul>
-  <input type="text" class="test" />
 </template>
 
 <script setup>
-//想要更換圖片，只要在slotList更改圖片(34x34)，就可自動更換。
 import { onMounted, ref } from "vue";
-const iNeedThisNode = ref();
+const nodeForArray = ref();
 const slotList = ref([
   {
     id: 0,
@@ -29,12 +27,11 @@ const slotList = ref([
   擬器過過乾癮，正好轉
   職的技能可以練習，藉
   此機會磨練自己的技能
-  ，也是真心想回憶衝裝
+  ，也是私心想回憶衝裝
   的天堂，打到人生第一
   張祝防的喜悅，開心沒
-  過多久，隔天裝備全消
-  失... 總之
-  
+  過多久，隔天就被盜帳
+  號...  總之
                         Enjoy it !`,
     },
   },
@@ -42,6 +39,7 @@ const slotList = ref([
     id: 1,
     name: "F6",
     src: "/src/assets/slot_white-armor.png",
+    toggle: true,
     toDisplay: {
       item: "對盔甲施法的卷軸",
       material: "紙",
@@ -52,10 +50,11 @@ const slotList = ref([
     id: 2,
     name: "F7",
     src: "/src/assets/slot_blessed-armor.png",
+    toggle: 1,
     toDisplay: {
       item: "對盔甲施法的卷軸",
       material: "紙",
-      weight: "- .",
+      weight: "-",
     },
   },
   {
@@ -65,7 +64,7 @@ const slotList = ref([
     toDisplay: {
       item: "對盔甲施法的卷軸",
       material: "紙",
-      weight: "∞",
+      weight: "-",
     },
   },
   {
@@ -85,7 +84,7 @@ const slotList = ref([
     toDisplay: {
       item: "對武器施法的卷軸",
       material: "紙",
-      weight: "防禦 5+0",
+      weight: "-",
     },
   },
   {
@@ -95,7 +94,7 @@ const slotList = ref([
     toDisplay: {
       item: "對武器施法的卷軸",
       material: "紙",
-      weight: "∞",
+      weight: "-",
     },
   },
   {
@@ -105,13 +104,13 @@ const slotList = ref([
     toDisplay: {
       item: "對武器施法的卷軸",
       material: "紙",
-      weight: "82",
+      weight: "-",
     },
   },
 ]);
 
 onMounted(function keydownToActives() {
-  const nodeList = Array.from(iNeedThisNode.value.children);
+  const nodeList = Array.from(nodeForArray.value.children);
   const whiteSlot = ["F6", "F10"];
   const blessedSlot = ["F7", "F11"];
   const cursedSlot = ["F8", "F12"];
@@ -130,14 +129,14 @@ onMounted(function keydownToActives() {
   });
 });
 function clickToActive(e) {
-  const nodeList = Array.from(iNeedThisNode.value.children);
+  const nodeList = Array.from(nodeForArray.value.children);
   nodeList.forEach((node) => {
     node.classList.remove("active");
     const targetClass = e.target.parentElement.classList[0];
     if (node.classList[0] === targetClass) node.classList.add("active");
   });
 }
-function getInfo(name, toDisplay) {
+function getInfoData(name, toDisplay) {
   // 這排版是故意的，white-space跟空白有關
   const content = `${toDisplay.item}
 材質:${toDisplay.material}
@@ -155,65 +154,46 @@ function checkSrc(imgSrc) {
 </script>
 
 <style scoped>
-.container {
+ul {
   position: absolute;
+  top: 81.55%;
+  left: 79.55%;
+
   display: grid;
   grid-template-columns: repeat(4, auto);
   grid-template-rows: repeat(2, auto);
-
   gap: 4px;
-  top: 81.55%;
-  left: 79.55%;
 }
+
 /* 統一排版位置：
-第0個開始做relative讓之後從第1~8個的.infoClass::after可以把(0)的relative做定位。*/
-.infoClass:nth-of-type(0) {
+第0個定位為relative，absolute做定位。*/
+.infoTemplate:nth-of-type(0) {
   position: relative;
 }
-
-.infoClass::after {
+.F5,
+.F9 {
+  /* 圖片顯示開關 */
+  opacity: 0;
+}
+.infoTemplate::after {
   content: attr(data-showInfo);
   position: absolute;
   top: -90%;
-  left: -4%;
-  /* 針對content的換行css語法 */
-  white-space: pre-wrap;
-
-  /* 裝備提示框樣式 */
-  display: none;
-  width: 150px;
-  height: 40px;
-  padding: 6px 0px 0px 3px;
-  background: rgba(0, 0, 0, 0.5);
-  border: 2px solid;
-  border-color: var(--color-white) var(--color-grey) var(--color-grey) var(--color-white);
-  font-family: serif, "Noto Serif TC";
-  font-weight: 400;
-  font-size: 12.5px;
-  line-height: 12.75px;
-  color: var(--color-white);
-}
-
-.infoClass:hover::after {
-  display: block;
-}
-
-.F5,
-.F9 {
+  left: -2%;
+  /* template顯示開關 */
   opacity: 0;
+  transition: opacity 0.3s ease-in 0.5s;
 }
 
-.F5::after {
-  content: “”;
-  height: 200px;
-  top: -310%;
-  line-height: 14px;
-  left: -1%;
+.infoTemplate:hover::after {
+  opacity: 1;
 }
 
+.F5::after,
 .F9::after {
-  height: 200px;
-  top: -310%;
+  top: -298%;
+  height: 190px;
+  line-height: 15px;
 }
 
 .F7::after,
@@ -227,12 +207,10 @@ function checkSrc(imgSrc) {
 }
 
 .active {
-  outline: 1.75px solid rgba(245, 217, 198, 0.83);
-  box-shadow: 0 0 2px 0px rgb(245, 217, 198, 0.83);
-  background-image: url("../assets/slot_empty.png");
   opacity: 1;
-}
-.test {
-  color: var(--color-red, orange);
+  background-image: url("../assets/slot_empty.png");
+  outline: 1.75px solid rgba(245, 217, 198, 0.83);
+
+  box-shadow: 0 0 2px 0px rgb(245, 217, 198, 0.83);
 }
 </style>
